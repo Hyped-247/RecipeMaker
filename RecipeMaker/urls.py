b477f63d7e3c736b0f1d9eeaa228cd6174a262ba
Mohammad Mahjoub <mmahjoub@westmont.edu>
2019-12-04 19:48:58 -0800

    Done.

--- RecipeMaker/urls.py
+++ RecipeMaker/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from recipes.api import RecipeListAPI
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('', include('recipes.urls')),
-    path('api/list/recipes/', RecipeListAPI.as_view()),
 ]
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path
 from rest_framework import routers
 from .api import RecipeViewSet
+from recipes.api import RecipeListAPI
 
 router = routers.DefaultRouter()
 router.register('api/recipes', RecipeViewSet, 'recipes')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('api/list/recipes/', RecipeListAPI.as_view()),
+]
+
+urlpatterns += router.urls
